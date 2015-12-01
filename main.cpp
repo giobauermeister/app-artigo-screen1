b3aab508c5c52b2730a5b8fc1ad6d9f37cf38811
Giovanni Santana <giovanni.santana@toradex.com>
2015-12-01 15:23:13 -0200

    bigger LED

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     screen1 w;
-    //w.show();
-    w.showFullScreen();
+    w.show();
+    //w.showFullScreen();
 
     return a.exec();
 }
